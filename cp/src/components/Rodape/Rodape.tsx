87a1848ae9d415d092b8ae45ea88c888cd57551c
gusandrini <gustavosandrini92@gmail.com>
2024-10-21 20:18:05 -0300

    adicionado conteúdo do footer

--- cp/src/components/Rodape/Rodape.tsx
+++ cp/src/components/Rodape/Rodape.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Insta from '../../Images/instagram.png'
 import Linke from '../../Images/linkedin.png';
+import Git from '../../Images/logotipo.png';
 
 export default function Rodape() {
     return (
@@ -9,27 +10,37 @@ export default function Rodape() {
             <div className="container-footer">
                 <div className="redes">
                     <h2>
-                        ACOMPANHE NOSSAS <br />
-                        PRINCIPAIS REDES SOCIAIS!
+                        Acompanhe as nossas principais Redes Sociais!
                     </h2>
 
                     <Image src={Insta} width={50} height={30} alt="logo instagram" />
 
                     <ul>
-                        <li><a className='instagram-Gu' target='_blank' rel='noopener' title='site Instagram' href="">Instagram Gu</a></li>
-                        <li><a className='instagram-Pizzi' target='_blank' rel='noopener' title='site Instagram' href="">Instagram Pizzi</a></li>
-                        <li><a className='instagram-Duda' target='_blank' rel='noopener' title='site Instagram' href="">Instagram Duda</a></li>
-                        <li><a className='instagram-Sora' target='_blank' rel='noopener' title='site Instagram' href="">Instagram Sora</a></li>
+                        <li><a className='instagram-Sora' target='_blank' rel='noopener' title='site Instagram' href="">Eduarda Tiemi</a></li>
+                        <li><a className='instagram-Pizzi' target='_blank' rel='noopener' title='site Instagram' href="">Felipe Pizzinato</a></li>
+                        <li><a className='instagram-Duda' target='_blank' rel='noopener' title='site Instagram' href="">Felipe Sora</a></li>
+                        <li><a className='instagram-Gu' target='_blank' rel='noopener' title='site Instagram' href="">Gustavo Sandrini</a></li>
                     </ul>
 
                     <Image src={Linke} width={50} height={30} alt="logo instagram" />
 
                     <ul>
-                        <li><a className='link-Gu' target='_blank' rel='noopener' title='site Linkedin' href="">Linkedin Gu</a></li>
-                        <li><a className='link-Pizzi' target='_blank' rel='noopener' title='site Linkedin' href="">Linkedin Pizzi</a></li>
-                        <li><a className='link-Duda' target='_blank' rel='noopener' title='site Linkedin' href="">Linkedin Duda</a></li>
-                        <li><a className='link-Sora' target='_blank' rel='noopener' title='site Linkedin' href="">Linkedin Sora</a></li>
+                        <li><a className='link-Sora' target='_blank' rel='noopener' title='site Linkedin' href="">Eduarda Tiemi</a></li>
+                        <li><a className='link-Pizzi' target='_blank' rel='noopener' title='site Linkedin' href="">Felipe Pizzinato</a></li>
+                        <li><a className='link-Duda' target='_blank' rel='noopener' title='site Linkedin' href="">Felipe Sora</a></li>
+                        <li><a className='link-Gu' target='_blank' rel='noopener' title='site Linkedin' href="">Gustavo Sandrini</a></li>
                     </ul>
+
+                    <Image src={Git} width={50} height={30} alt="logo instagram" />
+
+                    <ul>
+                        <li><a className='git-Sora' target='_blank' rel='noopener' title='site GitHub' href="">Eduarda Tiemi</a></li>
+                        <li><a className='git-Pizzi' target='_blank' rel='noopener' title='site GitHub' href="">Felipe Pizzinato</a></li>
+                        <li><a className='git-Duda' target='_blank' rel='noopener' title='site GitHub' href="">Felipe Sora</a></li>
+                        <li><a className='git-Gu' target='_blank' rel='noopener' title='site GitHub' href="">Gustavo Sandrini</a></li>
+                    </ul>
+
+
 
 
                 </div>
