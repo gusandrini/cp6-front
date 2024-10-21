d0a5ee8cfcef6ad6cae1f4f215a370473c677bb2
gusandrini <gustavosandrini92@gmail.com>
2024-10-21 17:40:01 -0300

    estruturando cabeçalho

--- cp/src/components/Cabecalho/Cabecalho.tsx
+++ cp/src/components/Cabecalho/Cabecalho.tsx
@@ -4,9 +4,18 @@ import Link from 'next/link';
 export default function Cabecalho() {
     return (
         <div>
-            <div className="container">
-                <p>cabecalho</p>
-            </div>
+            <header>
+                <div className="container">
+                    <nav>
+                        <ul>
+                            <li><Link href="/">Home</Link></li>
+                            <li><Link href="/">Home</Link></li>
+                            <li><Link href="/">Home</Link></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            
         </div>
     )
 }
