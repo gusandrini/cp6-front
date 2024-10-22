aafe08e84abf1f0d6375c50116dd989ad5a57d3c
gusandrini <gustavosandrini92@gmail.com>
2024-10-21 21:39:15 -0300

    adicionado parte do conteudo a pagina Challenge

--- cp/src/app/challenge/page.tsx
+++ cp/src/app/challenge/page.tsx
@@ -4,7 +4,16 @@ import Link from "next/link";
 export default function Challenge() {
     return (
         <div>
-            <p>challenge</p>
+            <div className="conteudo">
+                    <h1>Challenge Sprints</h1>
+                    <p>Os Challenges Sprints são avaliações focadas em resolução de problemas em prazos curtos e com entregas programadas. Esses desafios nos <br />
+                    colocam à prova, exigindo que aplicássemos nossos conhecimentos de forma ágil e eficiente, simulando o ritmo acelerado de ambientes <b>
+                        profissionais reais.
+                    </b>
+                    <br />
+                    Nesta página, apresentamos os Challenge Sprints que completamos ao longo do curso, destacando nossa capacidade de adaptação e <br />
+                    inovação ao enfrentar problemas práticos em um tempo limitado.</p>
+                </div>
         </div>
     )
 }
