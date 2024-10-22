7b77be7e1a30f1443bb9f1bf9308394c0d7377bd
felipesora <felipeusora@gmail.com>
2024-10-22 09:27:17 -0300

    Estilizando o rodapé

--- cp/src/components/Rodape/Rodape.tsx
+++ cp/src/components/Rodape/Rodape.tsx
@@ -1,47 +1,47 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import Insta from '../../Images/instagram.png'
-import Linke from '../../Images/linkedin.png';
-import Git from '../../Images/logotipo.png';
+import Instagram from '../../Images/instagram.png'
+import Linkedin from '../../Images/linkedin.png';
+import Github from '../../Images/logotipo.png';
 
 export default function Rodape() {
     return (
         <footer className='rodape'>
             <div className="container-footer">
-                <div className="redes">
+                <div className="redes_sociais">
                     <h2>
                         Acompanhe as nossas principais Redes Sociais!
                     </h2>
 
-                    <Image src={Insta} width={50} height={30} alt="logo instagram" />
-
-                    <ul>
-                        <li><a className='instagram-Sora' target='_blank' rel='noopener' title='site Instagram' href="">Eduarda Tiemi</a></li>
-                        <li><a className='instagram-Pizzi' target='_blank' rel='noopener' title='site Instagram' href="">Felipe Pizzinato</a></li>
-                        <li><a className='instagram-Duda' target='_blank' rel='noopener' title='site Instagram' href="">Felipe Sora</a></li>
-                        <li><a className='instagram-Gu' target='_blank' rel='noopener' title='site Instagram' href="">Gustavo Sandrini</a></li>
-                    </ul>
-
-                    <Image src={Linke} width={50} height={30} alt="logo instagram" />
-
-                    <ul>
-                        <li><a className='link-Sora' target='_blank' rel='noopener' title='site Linkedin' href="">Eduarda Tiemi</a></li>
-                        <li><a className='link-Pizzi' target='_blank' rel='noopener' title='site Linkedin' href="">Felipe Pizzinato</a></li>
-                        <li><a className='link-Duda' target='_blank' rel='noopener' title='site Linkedin' href="">Felipe Sora</a></li>
-                        <li><a className='link-Gu' target='_blank' rel='noopener' title='site Linkedin' href="">Gustavo Sandrini</a></li>
-                    </ul>
-
-                    <Image src={Git} width={50} height={30} alt="logo instagram" />
-
-                    <ul>
-                        <li><a className='git-Sora' target='_blank' rel='noopener' title='site GitHub' href="">Eduarda Tiemi</a></li>
-                        <li><a className='git-Pizzi' target='_blank' rel='noopener' title='site GitHub' href="">Felipe Pizzinato</a></li>
-                        <li><a className='git-Duda' target='_blank' rel='noopener' title='site GitHub' href="">Felipe Sora</a></li>
-                        <li><a className='git-Gu' target='_blank' rel='noopener' title='site GitHub' href="">Gustavo Sandrini</a></li>
-                    </ul>
-
-
-
+                    <div className='container_redes'>
+                        <div className='redes github'>
+                            <Image src={Github} width={50} height={30} alt="Logo do GitHub" />
+                            <ul>
+                                <li><a className='link_redes' target='_blank' rel='noopener' title='GitHub de Eduarda Tiemi' href="">Eduarda Tiemi</a></li>
+                                <li><a className='link_redes' target='_blank' rel='noopener' title='GitHub de Felipe Pizzinato' href="">Felipe Pizzinato</a></li>
+                                <li><a className='link_redes' target='_blank' rel='noopener' title='GitHub de Felipe Sora' href="">Felipe Sora</a></li>
+                                <li><a className='link_redes' target='_blank' rel='noopener' title='GitHub de Gustavo Sandrini' href="">Gustavo Sandrini</a></li>
+                            </ul>
+                        </div>
+                        <div className='redes instagram'>
+                            <Image src={Instagram} width={50} height={30} alt="Logo do Instagram" />
+                            <ul>
+                                <li><a className='link_redes' target='_blank' rel='noopener' title='Instagram de Eduarda Tiemi' href="">Eduarda Tiemi</a></li>
+                                <li><a className='link_redes' target='_blank' rel='noopener' title='Instagram de Felipe Pizzinato' href="">Felipe Pizzinato</a></li>
+                                <li><a className='link_redes' target='_blank' rel='noopener' title='Instagram de Felipe Sora' href="">Felipe Sora</a></li>
+                                <li><a className='link_redes' target='_blank' rel='noopener' title='Instagram de Gustavo Sandrini' href="">Gustavo Sandrini</a></li>
+                            </ul>
+                        </div>
+                        <div className='redes linkedin'>
+                            <Image src={Linkedin} width={50} height={30} alt="Logo do Linkedin" />
+                            <ul>
+                                <li><a className='link_redes' target='_blank' rel='noopener' title='Linkedin de Eduarda Tiemi' href="">Eduarda Tiemi</a></li>
+                                <li><a className='link_redes' target='_blank' rel='noopener' title='Linkedin de Felipe Pizzinato' href="">Felipe Pizzinato</a></li>
+                                <li><a className='link_redes' target='_blank' rel='noopener' title='Linkedin de Felipe Sora' href="">Felipe Sora</a></li>
+                                <li><a className='link_redes' target='_blank' rel='noopener' title='Linkedin de Gustavo Sandrini' href="">Gustavo Sandrini</a></li>
+                            </ul>
+                        </div>
+                    </div>
 
                 </div>
             </div>
