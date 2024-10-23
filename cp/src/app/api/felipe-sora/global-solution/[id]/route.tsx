dbff980fb1de103b44d63a0003c344eafceeb679
felipesora <felipeusora@gmail.com>
2024-10-23 17:14:11 -0300

    Todos os verbos HTTPS do Felipe Sora - Global Solution

---
+++ cp/src/app/api/felipe-sora/global-solution/[id]/route.tsx
@@ -0,0 +1,95 @@
+import { TipoTrabalhos } from "@/types";
+import { promises as fs } from "fs";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request, { params }: { params: { id: number } }) {
+    try {
+        // Carregar o arquivo JSON
+        const file = await fs.readFile(process.cwd() + '/src/data/felipe-sora.json', 'utf-8');
+        const data = JSON.parse(file);
+
+        // Certificar que o campo 'globalSolution' existe
+        if (data && data.globalSolution) {
+            // Encontrar o trabalho específico pelo ID nos 'globalSolution'
+            const trabalho = data.globalSolution.find((p: TipoTrabalhos) => p.id == params.id);
+
+            // Verificar se o trabalho foi encontrado
+            if (!trabalho) {
+                return NextResponse.json({ error: "Checkpoint não encontrado." });
+            }
+
+            // Retornar o checkpoint encontrado
+            return NextResponse.json(trabalho);
+        } else {
+            return NextResponse.json({ error: "Campo 'globalSolution' não encontrado no JSON." });
+        }
+    } catch (error) {
+        return NextResponse.json({ error: "Erro ao selecionar trabalho!: " + error });
+    }
+}
+
+export async function DELETE(request: Request, { params }: { params: { id: number } }) {
+    try {
+        // Carregar o arquivo JSON
+        const file = await fs.readFile(process.cwd() + '/src/data/felipe-sora.json', 'utf-8');
+        const data = JSON.parse(file);
+
+        // Certificar que o campo 'globalSolution' existe
+        if (data && data.globalSolution) {
+            // Encontrar o índice do item a ser deletado
+            const idTrabalho = data.globalSolution.findIndex((p: TipoTrabalhos) => p.id == params.id);
+
+            // Verificar se o item foi encontrado
+            if (idTrabalho === -1) {
+                return NextResponse.json({ error: "Trabalho não encontrado." });
+            }
+
+            // Remover o item do array
+            data.globalSolution.splice(idTrabalho, 1);
+
+            // Atualizar o arquivo JSON
+            const fileUpdate = JSON.stringify(data, null, 2); // Adicione 'null, 2' para formatar
+            await fs.writeFile(process.cwd() + '/src/data/felipe-sora.json', fileUpdate);
+
+            return NextResponse.json({ msg: "Trabalho removido com sucesso!" });
+        } else {
+            return NextResponse.json({ error: "Campo 'globalSolution' não encontrado no JSON." });
+        }
+    } catch (error) {
+        return NextResponse.json({ error: "Erro ao excluir o trabalho!: " + error });
+    }
+}
+
+export async function PUT(request: Request, { params }: { params: { id: number } }) {
+    try {
+        // Carregar o arquivo JSON
+        const file = await fs.readFile(process.cwd() + '/src/data/felipe-sora.json', 'utf-8');
+        const data = JSON.parse(file);
+
+        // Certificar que o campo 'globalSolution' existe
+        if (data && data.globalSolution) {
+            // Encontrar o índice do checkpoint a ser atualizado
+            const indice = data.globalSolution.findIndex((p: TipoTrabalhos) => p.id == params.id);
+
+            if (indice !== -1) {
+                // Obter os dados atualizados do corpo da requisição
+                const trabalhoAtualizado: TipoTrabalhos = await request.json();
+
+                // Atualizar o checkpoint no índice encontrado
+                data.globalSolution[indice] = { ...data.globalSolution[indice], ...trabalhoAtualizado };
+
+                // Atualizar o arquivo JSON
+                const fileUpdate = JSON.stringify(data, null, 2); // Adicione um `null, 2` para formatação legível
+                await fs.writeFile(process.cwd() + '/src/data/felipe-sora.json', fileUpdate);
+
+                return NextResponse.json({ msg: "globalSolution atualizado com sucesso!" });
+            } else {
+                return NextResponse.json({ error: "globalSolution não encontrado." });
+            }
+        } else {
+            return NextResponse.json({ error: "Campo 'globalSolution' não encontrado no JSON." });
+        }
+    } catch (error) {
+        return NextResponse.json({ error: "Erro ao atualizar trabalho!: " + error });
+    }
+}
--- cp/src/app/integrantes/sora/sora-gs/page.tsx
+++ cp/src/app/integrantes/sora/sora-gs/page.tsx
@@ -1,10 +1,85 @@
+"use client";
+import { TipoTrabalhos } from "@/types";
+import { useEffect, useState } from "react";
+import { useRouter } from 'next/navigation';
+import Image from "next/image";
+import Link from 'next/link';
+
+import iconeCheck from "@/Images/icone-check.png";
+import iconeEditar from "@/Images/icone-editar.png";
+import iconeApagar from "@/Images/icone-apagar.png";
+import iconeAdicionar from "@/Images/icone-adicionar.png";
+
 
 export default function Sora() {
+
+    const navigate = useRouter();
+
+    const [lista, setLista] = useState<TipoTrabalhos[]>([]);
+
+    useEffect(() => {
+        const chamadaApi = async () => {
+            try {
+                const response = await fetch("http://localhost:3000/api/felipe-sora/global-solution");
+                const data = await response.json();
+                console.log(data);
+
+                // Filtrando apenas os dados de 'globalSolution'
+                if (data && data.globalSolution) {
+                    setLista(data.globalSolution);
+                } else {
+                    console.error("Dados de globalSolution não encontrados.");
+                }
+            } catch (error) {
+                console.error("Erro ao buscar dados:", error);
+            }
+        };
+        chamadaApi();
+    }, []);
+
+    const handleDelete = async (id: number) => {
+        try {
+            const response = await fetch(`http://localhost:3000/api/felipe-sora/global-solution/${id}`, {
+                method: 'DELETE',
+            });
+    
+            if (response.ok) {
+                alert("Trabalho removido com sucesso!");
+                // Atualizar a lista sem navegar para outra página
+                setLista(prev => prev.filter(trabalho => trabalho.id !== id));
+            } else {
+                alert("Falha ao remover o trabalho!");
+            }
+        } catch (error) {
+            console.error("Falha ao remover trabalho.", error);
+        }
+    };
+    
+
+
     return (
-        <div>
-            <div className="container">
-                <p>sora -gs</p>
+        <div className="container_trabalhos">
+            <h1>GlobalSolution - Felipe Sora</h1>
+            {Array.isArray(lista) && lista.map((trabalho) => (
+                <div key={trabalho.id} className="caixa_trabalhos">
+                    <Image src={iconeCheck} alt="Icone de Check" className="icone_check"/>
+                    <div>
+                        <p className="texto_caixa">Nome do Projeto: {trabalho.nome}</p>
+                        <p className="texto_caixa">Descrição: {trabalho.descricao}</p>
+                        <p className="texto_caixa">Matéria: {trabalho.materia}</p>
+                        <div className="texto_icones_caixa">
+                            <p className="texto_caixa">Nota: {trabalho.nota}</p>
+                            <div className="icones_caixa">
+                                <Link href={`/integrantes/sora/sora-gs/gs/${trabalho.id}`} className="botoes_trabalhos"><Image src={iconeEditar} alt="Icone de Editar" className="icones"/></Link>
+                                <Link href="#" onClick={() => handleDelete(trabalho.id)}  className="botoes_trabalhos"><Image src={iconeApagar} alt="Icone de Apagar" className="icones"/></Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+            <div className="caixa_adicionar">
+                <Link href="/integrantes/sora/sora-gs/cad-gs-sora" className="links_adiconar"><Image src={iconeAdicionar} alt="Icone de Adicionar" className="icone_adicionar"/></Link>
             </div>
         </div>
-    )
+    );
 }
