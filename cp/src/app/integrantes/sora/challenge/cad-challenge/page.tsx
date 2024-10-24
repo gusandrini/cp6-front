3358fe1f0fde7d77e1d9a9c69f260e68f4baa7de
felipesora <felipeusora@gmail.com>
2024-10-24 09:06:41 -0300

    Arrumando placeholder do formulario

--- cp/src/app/integrantes/sora/challenge/cad-challenge/page.tsx
+++ cp/src/app/integrantes/sora/challenge/cad-challenge/page.tsx
@@ -45,25 +45,25 @@ export default function CadCpSora() {
 
     return(
         <div className="container_cadastro">
-            <h1>Cadastro de CheckPoint</h1>
+            <h1>Cadastro de Challenge</h1>
                 <form onSubmit={handleSubmit} className="container_formulario">
                     <div className="label_input">
-                        <label htmlFor="idNome">Nome do Checkpoint:</label>
-                        <input type="text" name="nome" id="idNome" className="text-black"  value={trabalho.nome} onChange={(e)=> setTrabalho({...trabalho,nome:e.target.value})} placeholder="Nome do CheckPoint"/>
+                        <label htmlFor="idNome">Nome do Challenge:</label>
+                        <input type="text" name="nome" id="idNome" className="text-black"  value={trabalho.nome} onChange={(e)=> setTrabalho({...trabalho,nome:e.target.value})} placeholder="Nome do Challenge"/>
                     </div>
 
                     <div className="label_input">
-                        <label htmlFor="idDescricao">Descrição do Checkpoint:</label>
-                        <input type="text" name="descricao" id="idDescricao" className="text-black" value={trabalho.descricao} onChange={(e)=> setTrabalho({...trabalho,descricao:e.target.value})} placeholder="Descrição do CheckPoint"/>
+                        <label htmlFor="idDescricao">Descrição do Challenge:</label>
+                        <input type="text" name="descricao" id="idDescricao" className="text-black" value={trabalho.descricao} onChange={(e)=> setTrabalho({...trabalho,descricao:e.target.value})} placeholder="Descrição do Challenge"/>
                     </div>
 
                     <div className="label_input">
-                        <label htmlFor="idMateria">Matéria do CheckPoint:</label>
-                        <input type="text" name="materia" id="idMateria" className="text-black" value={trabalho.materia} onChange={(e)=> setTrabalho({...trabalho,materia:e.target.value})} placeholder="Matéria do CheckPoint"/>
+                        <label htmlFor="idMateria">Matéria do Challenge:</label>
+                        <input type="text" name="materia" id="idMateria" className="text-black" value={trabalho.materia} onChange={(e)=> setTrabalho({...trabalho,materia:e.target.value})} placeholder="Matéria do Challenge"/>
                     </div>
 
                     <div className="label_input">
-                        <label htmlFor="idNota">Nota do CheckPoint:</label>
+                        <label htmlFor="idNota">Nota do Challenge:</label>
                         <input type="number" name="nota" id="idNota" className="text-black" value={trabalho.nota} onChange={(e)=> setTrabalho({...trabalho,nota: parseInt(e.target.value) })}/>
                     </div>
 
