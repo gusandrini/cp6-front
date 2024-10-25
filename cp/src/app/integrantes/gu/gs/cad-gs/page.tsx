921f34b5bcfe91d1a3439fd61a2e084b041db075
gusandrini <gustavosandrini92@gmail.com>
2024-10-25 16:45:11 -0300

    criação de formulários e verbos https

---
+++ cp/src/app/integrantes/gu/gs/cad-gs/page.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { TipoTrabalhos } from "@/types";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function CadCpGustavo() {
+
+    const navigate = useRouter();
+
+    const [trabalho, setTrabalho] = useState<TipoTrabalhos>({
+        id:0,
+        nome: "",
+        descricao: "",
+        materia: "",
+        nota: 0,
+      });
+
+      const handleSubmit = async (e:React.FormEvent<HTMLFormElement>)=> {
+
+        e.preventDefault();
+
+        try {
+            const response = await fetch("http://localhost:3000/api/gustavo/global-solution",{
+                method: "POST",
+                headers:{
+                  "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(trabalho),
+              });
+
+            if(response.ok) {
+                setTrabalho({id:0,nome:"",descricao:"",materia:"", nota: 0});
+                alert("Trabalho cadastrado com sucesso!");
+                navigate.push("/integrantes/gu/gs");
+            } else{
+                alert("Erro ao cadastrar produto!");
+            }
+
+
+        } catch (error) {
+            console.error("Erro ao cadastrar trabalho: " , error);
+        }
+      }
+
+    return(
+        <div className="container_cadastro">
+            <h1>Cadastro de Global Solution</h1>
+                <form onSubmit={handleSubmit} className="container_formulario">
+                    <div className="label_input">
+                        <label htmlFor="idNome">Nome do Global Solution:</label>
+                        <input type="text" name="nome" id="idNome" className="text-black"  value={trabalho.nome} onChange={(e)=> setTrabalho({...trabalho,nome:e.target.value})} placeholder="Nome do Global Solution"/>
+                    </div>
+
+                    <div className="label_input">
+                        <label htmlFor="idDescricao">Descrição do Global Solution:</label>
+                        <input type="text" name="descricao" id="idDescricao" className="text-black" value={trabalho.descricao} onChange={(e)=> setTrabalho({...trabalho,descricao:e.target.value})} placeholder="Descrição do Global Solution"/>
+                    </div>
+
+                    <div className="label_input">
+                        <label htmlFor="idMateria">Matéria do Global Solution:</label>
+                        <input type="text" name="materia" id="idMateria" className="text-black" value={trabalho.materia} onChange={(e)=> setTrabalho({...trabalho,materia:e.target.value})} placeholder="Matéria do Global Solution"/>
+                    </div>
+
+                    <div className="label_input">
+                        <label htmlFor="idNota">Nota do Global Solution:</label>
+                        <input type="number" name="nota" id="idNota" className="text-black" value={trabalho.nota} onChange={(e)=> setTrabalho({...trabalho,nota: parseInt(e.target.value) })}/>
+                    </div>
+
+                    <div>
+                        <button type="submit" className="botao_cadastrar">Cadastrar</button>
+                    </div>
+                </form>
+        </div>
+    );
+}
--- cp/src/app/integrantes/gu/gs/page.tsx
+++ cp/src/app/integrantes/gu/gs/page.tsx
@@ -1,11 +1,83 @@
-import Link from "next/link";
+"use client";
+import { TipoTrabalhos } from "@/types";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from 'next/link';
 
-export default function Gu() {
+import iconeCheck from "@/Images/icone-check.png";
+import iconeEditar from "@/Images/icone-editar.png";
+import iconeApagar from "@/Images/icone-apagar.png";
+import iconeAdicionar from "@/Images/icone-adicionar.png";
+
+
+export default function Gustavo() {
+
+
+    const [lista, setLista] = useState<TipoTrabalhos[]>([]);
+
+    useEffect(() => {
+        const chamadaApi = async () => {
+            try {
+                const response = await fetch("http://localhost:3000/api/gustavo/global-solution");
+                const data = await response.json();
+                console.log(data);
+
+                // Filtrando apenas os dados de 'globalSolution'
+                if (data && data.globalSolution) {
+                    setLista(data.globalSolution);
+                } else {
+                    console.error("Dados de globalSolution não encontrados.");
+                }
+            } catch (error) {
+                console.error("Erro ao buscar dados:", error);
+            }
+        };
+        chamadaApi();
+    }, []);
+
+    const handleDelete = async (id: number) => {
+        try {
+            const response = await fetch(`http://localhost:3000/api/gustavo/global-solution/${id}`, {
+                method: 'DELETE',
+            });
+    
+            if (response.ok) {
+                alert("Trabalho removido com sucesso!");
+                // Atualizar a lista sem navegar para outra página
+                setLista(prev => prev.filter(trabalho => trabalho.id !== id));
+            } else {
+                alert("Falha ao remover o trabalho!");
+            }
+        } catch (error) {
+            console.error("Falha ao remover trabalho.", error);
+        }
+    };
+    
+
+
     return (
-        <div>
-            <div className="container">
-                <p>gu gs</p>
+        <div className="container_trabalhos">
+            <h1>GlobalSolution - Gustavo Sandrini</h1>
+            {Array.isArray(lista) && lista.map((trabalho) => (
+                <div key={trabalho.id} className="caixa_trabalhos">
+                    <Image src={iconeCheck} alt="Icone de Check" className="icone_check"/>
+                    <div>
+                        <p className="texto_caixa">Nome do Projeto: {trabalho.nome}</p>
+                        <p className="texto_caixa">Descrição: {trabalho.descricao}</p>
+                        <p className="texto_caixa">Matéria: {trabalho.materia}</p>
+                        <div className="texto_icones_caixa">
+                            <p className="texto_caixa">Nota: {trabalho.nota}</p>
+                            <div className="icones_caixa">
+                                <Link href={`/integrantes/gu/gs/gs/${trabalho.id}`} className="botoes_trabalhos"><Image src={iconeEditar} alt="Icone de Editar" className="icones"/></Link>
+                                <Link href="#" onClick={() => handleDelete(trabalho.id)}  className="botoes_trabalhos"><Image src={iconeApagar} alt="Icone de Apagar" className="icones"/></Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+            <div className="caixa_adicionar">
+                <Link href="/integrantes/gu/gs/cad-gs" className="links_adiconar"><Image src={iconeAdicionar} alt="Icone de Adicionar" className="icone_adicionar"/></Link>
             </div>
         </div>
-    )
+    );
 }
