f14e20bcb43c1463a73e064ec18b03c29ecf8d04
gusandrini <gustavosandrini92@gmail.com>
2024-10-25 16:43:38 -0300

    criação dos verbos https-Gustavo

---
+++ cp/src/app/api/gustavo/checkpoint/route.tsx
@@ -0,0 +1,49 @@
+import { NextResponse } from "next/server";
+import { promises as fs } from "fs";
+import { TipoTrabalhos } from "@/types";
+import { log } from "console";
+
+// Métodos GET
+
+export async function GET() {
+    try {
+        const file = await fs.readFile(process.cwd() + '/src/data/gustavo.json', 'utf-8');
+        const trabalhos = JSON.parse(file);
+
+        return NextResponse.json(trabalhos);
+
+    } catch (error) {
+        return NextResponse.json({ error: "Erro ao recuperar os dados!: " + error });
+    }
+}
+
+
+// Método POST CHECKPOINTS FELIPE SORA
+export async function POST(request: Request) {
+    try {
+        const file = await fs.readFile(process.cwd() + '/src/data/gustavo.json', 'utf-8');
+        const trabalhos = JSON.parse(file);
+
+        const trabalho: TipoTrabalhos = await request.json();
+
+        // Verifica se o campo checkpoints existe
+        if (!Array.isArray(trabalhos.checkpoints)) {
+            return NextResponse.json({ error: "A lista de checkpoints não foi encontrada." }, { status: 404 });
+        }
+
+        // Incrementa o id
+        trabalho.id = (trabalhos.checkpoints[trabalhos.checkpoints.length - 1].id + 1);
+
+        // Adiciona o novo trabalho à lista de checkpoints
+        trabalhos.checkpoints.push(trabalho);
+
+        // Converte a lista de volta para string
+        const fileUpdate = JSON.stringify(trabalhos, null, 2); // Adiciona formatação para melhor legibilidade
+        await fs.writeFile(process.cwd() + '/src/data/gustavo.json', fileUpdate);
+
+        return NextResponse.json(trabalho);
+
+    } catch (error) {
+        return NextResponse.json({ error: "Erro ao cadastrar Trabalho!: " + error });
+    }
+}
