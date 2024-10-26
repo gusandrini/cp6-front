d4daf9118df6d3a2233c7b16e2c1a2ff15ab2078
felipesora <felipeusora@gmail.com>
2024-10-26 17:53:34 -0300

    Pagina de erro

--- cp/src/app/not-found.tsx
+++ cp/src/app/not-found.tsx
@@ -1,8 +1,12 @@
 import Link from 'next/link';
+import Image from "next/image";
+
+import fotoErro from "@/Images/error-404.png";
 
 export default function NotFound() {
   return (
     <div className="NotFound">
+      <Image src={fotoErro} alt="Imagem de erro 404" className="foto_erro"/>
       <Link href="/">
         Voltar para a página inicial
       </Link>
